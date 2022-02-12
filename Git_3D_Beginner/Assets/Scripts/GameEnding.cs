f7fa0709922e653e1b567caa81d5622bf11d13f8
MoriCesar <cesinha_lowlife@hotmail.com>
2022-02-12 21:23:05 +0900

    tutorial_6

--- Git_3D_Beginner/Assets/Scripts/GameEnding.cs
+++ Git_3D_Beginner/Assets/Scripts/GameEnding.cs
@@ -1,42 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
-   public float fadeDuration = 1f;
-   public float displayImageDuration = 1f;
-   public GameObject player;
-   public CanvasGroup exitBackgroundImageCanvasGroup;
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup; 
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
-   bool m_IsPlayerAtExit;
-   float m_Timer;
-
-
-   void OnTriggerEnter(Collider other)
-   { 
-       if(other.gameObject == player)
-       { 
-           m_IsPlayerAtExit = true;
-       }
-   }
-
-   void Update()
-   { 
-       if(m_IsPlayerAtExit)
-       { 
-           EndLevel();
-       }
-   }
-
-   void EndLevel()
-   {   
-        m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-
-        if(m_Timer > fadeDuration + displayImageDuration)
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
         {
-            Application.Quit ();
+            m_IsPlayerAtExit = true;
         }
-   }
+    }
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
+        }
+    }
 }
+
